c673684fe04aa1df8b2a9c11e71824b5861890e6
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-10-10 09:29:21 -0700

    .

--- src/components/OneProblem.js
+++ src/components/OneProblem.js
@@ -2,7 +2,7 @@ import React from "react";
 import api from "../services/api";
 import { Route, Switch, Link, NavLink, withRouter } from "react-router-dom";
 import { addNewComment } from "../actions";
-
+import NewComment from "./NewComment";
 import { connect } from "react-redux";
 
 class OneProblem extends React.Component {
@@ -10,10 +10,11 @@ class OneProblem extends React.Component {
     problem: {},
     dependent: {},
     comments: [],
-    newComment: {
-      text: "",
-      status_open: false,
-    },
+    // newComment: {
+    //   text: "",
+    //   status_open: false,
+    // },
+    isEditing: false,
   };
   componentDidMount() {
     if (!localStorage.token) {
@@ -30,54 +31,54 @@ class OneProblem extends React.Component {
     }
   }
 
-  handleInputChange = () => {
-    this.setState({
-      ...this.state,
-      newComment: {
-        ...this.state.newComment,
-        status_open: !this.state.newComment.status_open,
-      },
-    });
-  };
-  handleInput = (e) => {
-    this.setState({
-      ...this.state,
-      newComment: {
-        ...this.state.newComment,
-        text: e.target.value,
-      },
-    });
-  };
+  // handleInputChange = () => {
+  //   this.setState({
+  //     ...this.state,
+  //     newComment: {
+  //       ...this.state.newComment,
+  //       status_open: !this.state.newComment.status_open,
+  //     },
+  //   });
+  // };
+  // handleInput = (e) => {
+  //   this.setState({
+  //     ...this.state,
+  //     newComment: {
+  //       ...this.state.newComment,
+  //       text: e.target.value,
+  //     },
+  //   });
+  // };
 
-  updateOneProblemPage = (e) => {
-    const { problem, newComment, comments } = this.state;
-    e.preventDefault();
-    let today = Date.now();
-    let time = new Intl.DateTimeFormat("en-US", {
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-    }).format(today);
+  // updateOneProblemPage = (e) => {
+  //   const { problem, newComment, comments } = this.state;
+  //   e.preventDefault();
+  //   let today = Date.now();
+  //   let time = new Intl.DateTimeFormat("en-US", {
+  //     year: "numeric",
+  //     month: "2-digit",
+  //     day: "2-digit",
+  //     hour: "2-digit",
+  //     minute: "2-digit",
+  //     second: "2-digit",
+  //   }).format(today);
 
-    this.setState({
-      ...this.state,
-      comments: [
-        ...comments,
-        {
-          created_at: time,
-          id: 0,
-          problem_id: problem.id,
-          status_open: newComment.status_open,
-          text: newComment.text,
-          updated_at: time,
-        },
-      ],
-    });
-    this.props.addNewComment(newComment, e, problem.id);
-  };
+  //   this.setState({
+  //     ...this.state,
+  //     comments: [
+  //       ...comments,
+  //       {
+  //         created_at: time,
+  //         id: 0,
+  //         problem_id: problem.id,
+  //         status_open: newComment.status_open,
+  //         text: newComment.text,
+  //         updated_at: time,
+  //       },
+  //     ],
+  //   });
+  //   this.props.addNewComment(newComment, e, problem.id);
+  // };
 
   handleEditComment = (e) => {
     console.log(e.target.id);
@@ -100,7 +101,10 @@ class OneProblem extends React.Component {
           <div className="problem-container-buttons">
             <button className="btn">Upload File</button>
             <button className="btn">Add New Appointment</button>
-            <form onSubmit={(e) => this.updateOneProblemPage(e)}>
+            <div>
+              <NewComment />
+            </div>
+            {/* <form onSubmit={(e) => this.updateOneProblemPage(e)}>
               <label>
                 {" "}
                 Status
@@ -120,7 +124,7 @@ class OneProblem extends React.Component {
                 onChange={this.handleInput}
               />
               <button className="btn">Add New Comment</button>
-            </form>
+            </form> */}
           </div>
         </div>
         <br />
