c5cbf49dce820547eabdb13c763fda80bd99c8ef
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-10-16 16:23:24 -0700

    delete notes from AllFilesList

--- src/components/files/AllFilesList.js
+++ src/components/files/AllFilesList.js
@@ -1,31 +1,26 @@
 import React from "react";
 import { deleteFile } from "../../actions";
 import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
-import api from "../../services/api";
 
-class OneProbComments extends React.Component {
+class AllFilesList extends React.Component {
   handleDeleteFile = (problem, file) => {
     const problem_id = problem.id;
-    // debugger;
     const index = problem.file.findIndex((f) => f == file);
-    // console.log(index);
     this.props.deleteFile(problem_id, index);
-    // api.problems
-    //   .deleteFile(problem_id, index)
-    //   .then((data) => console.log(data));
   };
   render() {
     let problem = this.props.id[0];
-    console.log(problem);
     return (
       <div>
         <div className="one-problem-comments">
           <h1>File List</h1>
           {problem
             ? problem.file.map((file) => (
-                <li className="x-btn" key={file.id}>
-                  <a href={`http://localhost:3000/${file}`} target="_blank">
+                <li key={file.id}>
+                  <a
+                    href={`http://localhost:3000/${file.path}`}
+                    target="_blank"
+                  >
                     {file.name}
                   </a>
                   <button onClick={() => this.handleDeleteFile(problem, file)}>
@@ -40,19 +35,10 @@ class OneProbComments extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    //     // comments: state.comments,
-    //     problems: state.problems,
-  };
-};
 const mapDispatchToProps = (dispatch) => {
   return {
     deleteFile: (problem_id, index) => dispatch(deleteFile(problem_id, index)),
   };
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(withRouter(OneProbComments));
+export default connect(null, mapDispatchToProps)(AllFilesList);
