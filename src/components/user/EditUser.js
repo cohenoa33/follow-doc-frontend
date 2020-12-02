f239b4a496679c13fc30253a004d0aadcd23f86c
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-12-02 18:30:07 +0200

    added button for change pasword

--- src/components/user/EditUser.js
+++ src/components/user/EditUser.js
@@ -6,6 +6,7 @@ import { authorized } from "../../services/helpers";
 
 class EditUser extends React.Component {
   state = {
+    changePassword: false,
     password: "",
     password_confirmation: "",
     blockInput: false,
@@ -13,6 +14,9 @@ class EditUser extends React.Component {
   componentDidMount() {
     authorized(this.props.history);
   }
+  changePassword = () => {
+    this.setState({ changePassword: !this.state.changePassword });
+  };
 
   handleChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
@@ -42,40 +46,54 @@ class EditUser extends React.Component {
         {!this.props.user.user ? (
           <div> Loading... </div>
         ) : (
-          <div className="column-100">
-            {this.state.blockInput ? (
-              <div className="success-message">
-                {" "}
-                <h1>Your Password Has Been Changed Successfully</h1>
+          <>
+            {this.state.changePassword ? (
+              <div className="column-100">
+                {this.state.blockInput ? (
+                  <div className="success-message">
+                    {" "}
+                    <h1>Your Password Has Been Changed Successfully</h1>
+                  </div>
+                ) : (
+                  <div>
+                    <h1 className="h1-title">Change password</h1>
+                    <p>
+                      It's a good idea to use a strong password that you're not
+                      using elsewhere
+                    </p>
+                    <form onSubmit={this.handleSubmit}>
+                      <input
+                        type="password"
+                        onChange={this.handleChange}
+                        value={this.state.password}
+                        name="password"
+                        placeholder="New Password"
+                        required
+                      ></input>
+                      <input
+                        type="password"
+                        onChange={this.handleChange}
+                        name="password_confirmation"
+                        placeholder="Re-type New Password"
+                        value={this.state.password_confirmation}
+                        required
+                      />
+                      <button className="btn">Save Changes</button>
+                    </form>
+                    <div>
+                      <br></br>
+                    </div>
+                  </div>
+                )}
               </div>
             ) : (
               <div>
-                <h1 className="h1-title">Change Your Password</h1>
-                <form onSubmit={this.handleSubmit}>
-                  <input
-                    type="password"
-                    onChange={this.handleChange}
-                    value={this.state.password}
-                    name="password"
-                    placeholder="Password"
-                    required
-                  ></input>
-                  <input
-                    type="password"
-                    onChange={this.handleChange}
-                    name="password_confirmation"
-                    placeholder="Password Confirmation"
-                    value={this.state.password_confirmation}
-                    required
-                  />
-                  <button className="btn">Update</button>
-                </form>
-                <div>
-                  <br></br>
-                </div>
+                <button className="btn-width-80" onClick={this.changePassword}>
+                  Change Password
+                </button>
               </div>
             )}
-          </div>
+          </>
         )}
       </div>
     );
